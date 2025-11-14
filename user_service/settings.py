c84a84fc8a89fc45e4e0d5b24eba5728571d3ddb
michaelborhlar <michealadefehinti09@gmail.com>
2025-11-14 12:51:57 +0100

    Update settings.py

--- user_service/settings.py
+++ user_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.auth',
